744854ee50f660631004facf0307c5b7a814e90b
Alec Deitloff <alec.deitloff@gmail.com>
2016-12-10 16:25:37 -0800

    Added the majority of a control for displaying Type badges the way that we have them in the games, but these can be scaled and are actual WPF controls. The only part left is to just populate the colours (which is a p time expensive task). Also, I think that the top and bottom borders for the badge are actually just opacity layers of white and black, and that would cut down on the number of converters that we have by a lot if we can find out what those are. Investigate in subsequent commit.

---
+++ TrainersSchool/Controls/TypeIcon.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TrainersSchool.Pokemon;
+
+namespace TrainersSchool.Controls
+{
+	/// <summary>
+	/// Interaction logic for TypeIcon.xaml
+	/// </summary>
+	public partial class TypeIcon : UserControl
+	{
+		public TypeIcon()
+		{
+			InitializeComponent();
+		}
+
+		public PokemonType Type
+		{
+			get { return (PokemonType) GetValue( TypeProperty ); }
+			set { SetValue( TypeProperty, value ); }
+		}
+
+		public static readonly DependencyProperty TypeProperty = DependencyProperty.Register( "Type", typeof( PokemonType ), typeof( TypeIcon ),
+			new PropertyMetadata( PokemonType.Water ) );
+	}
+
+	class TypeToBrushConverter : IValueConverter
+	{
+		public Object Convert( Object value, Type targetType, Object parameter, CultureInfo culture )
+		{
+			switch ( (PokemonType) value )
+			{
+				case PokemonType.Normal:
+					return NormalBrush;
+				case PokemonType.Fighting:
+					return FightingBrush;
+				case PokemonType.Flying:
+					return FlyingBrush;
+				case PokemonType.Poison:
+					return PoisonBrush;
+				case PokemonType.Ground:
+					return GroundBrush;
+				case PokemonType.Rock:
+					return RockBrush;
+				case PokemonType.Bug:
+					return BugBrush;
+				case PokemonType.Ghost:
+					return GhostBrush;
+				case PokemonType.Steel:
+					return SteelBrush;
+				case PokemonType.Fire:
+					return FireBrush;
+				case PokemonType.Water:
+					return WaterBrush;
+				case PokemonType.Grass:
+					return GrassBrush;
+				case PokemonType.Electric:
+					return ElectricBrush;
+				case PokemonType.Psychic:
+					return PsychicBrush;
+				case PokemonType.Ice:
+					return IceBrush;
+				case PokemonType.Dragon:
+					return DragonBrush;
+				case PokemonType.Dark:
+					return DarkBrush;
+				case PokemonType.Fairy:
+					return FairyBrush;
+				default:
+					throw new ArgumentException();
+			}
+		}
+
+		public Object ConvertBack( Object value, Type targetType, Object parameter, CultureInfo culture )
+		{
+			throw new NotSupportedException();
+		}
+
+		public Brush NormalBrush { get; set; }
+		public Brush FightingBrush { get; set; }
+		public Brush FlyingBrush { get; set; }
+		public Brush PoisonBrush { get; set; }
+		public Brush GroundBrush { get; set; }
+		public Brush RockBrush { get; set; }
+		public Brush BugBrush { get; set; }
+		public Brush GhostBrush { get; set; }
+		public Brush SteelBrush { get; set; }
+		public Brush FireBrush { get; set; }
+		public Brush WaterBrush { get; set; }
+		public Brush GrassBrush { get; set; }
+		public Brush ElectricBrush { get; set; }
+		public Brush PsychicBrush { get; set; }
+		public Brush IceBrush { get; set; }
+		public Brush DragonBrush { get; set; }
+		public Brush DarkBrush { get; set; }
+		public Brush FairyBrush { get; set; }
+	}
+}
