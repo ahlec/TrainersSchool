b2218a3cae0737fdbf26ec6c05e2ed1ad4fec44e
Alec Deitloff <alec.deitloff@gmail.com>
2016-12-12 22:56:01 -0800

    Did some more UI work: + Cleaned up the sidebar answer history so that it's more compact. Still need to work on communicating the information to the player better, but it's much better now. + Added a top bar to the session view window - Resized the window some more

---
+++ TrainersSchool/Converters/BooleanToBrushConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TrainersSchool.Converters
+{
+	public class BooleanToBrushConverter : IValueConverter
+	{
+		public Object Convert( Object value, Type targetType, Object converterParameter, CultureInfo culture )
+		{
+			return ( (Boolean) value ? TrueBrush : FalseBrush );
+		}
+
+		public Object ConvertBack( Object value, Type targetType, Object converterParameter, CultureInfo culture )
+		{
+			throw new NotSupportedException();
+		}
+
+		public Brush TrueBrush { get; set; }
+
+		public Brush FalseBrush { get; set; }
+	}
+}
